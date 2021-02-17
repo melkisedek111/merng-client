adb2511786991032c257de4faaa9cb3e55930a7f
Melkisedek Ubalde <melkisedek222@gmail.com>
2021-02-17 14:08:19 +0800

    edit proxy and server base url

--- src/ApolloProvider.jsx
+++ src/ApolloProvider.jsx
@@ -7,7 +7,7 @@ import { ApolloClient,
 } from "@apollo/client";
 
 const httpLink = createHttpLink({
-	uri: "http://localhost:5000",
+	uri: "https://agile-earth-37840.herokuapp.com/",
 });
 
 const authLink = new ApolloLink((operation, forward) => {
